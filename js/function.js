e869b91baaf0d8f1e355e12cbcf0b3e19b37887e
Victor Wesley <vic.devjs@gmail.com>
2025-03-28 17:47:24 -0300

    versão 1.4.6.2 (inicio do carrosel)

--- js/function.js
+++ js/function.js
@@ -1,3 +1,32 @@
 let cards = document.querySelector('#modelos-cont');
+let item = cards.children;
+let screen = window.screen.width;
+let left = 45;
 
-console.log(cards)
+for (let i = 0; i < item.length; i++) {
+    item[i].innerHTML = `item ${i}`
+}
+
+function retornar() {
+    if (left <= -900) {
+        left = 45
+        cards.style.left = `${left}px`;
+    }
+};
+
+function turnRight() {
+    if (screen >= 1400) {
+        left = left - 225;
+        cards.style.left = `${left}px`;
+        retornar();
+    }else if (screen >= 800) {
+        
+    } else {
+        
+    }
+    console.log(left);
+    
+}
+
+
+console.log(screen);
