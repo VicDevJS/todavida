b4cdc79be794c3a6f8c878cbdd38711a8600286c
Victor Wesley <vic.devjs@gmail.com>
2025-04-06 23:20:19 -0300

    versão 1.5 (finalização da sessão catálogo, com botões funcionais)

--- js/function.js
+++ js/function.js
@@ -2,6 +2,7 @@ let cards = document.querySelector('#modelos-cont');
 let item = cards.children;
 let screen = window.screen.width;
 let left;
+let right;
 
 if (screen >= 426) {
     left = 40;
@@ -17,28 +18,33 @@ if (screen >= 426) {
     cards.style.left = `${left}px`
 }
 
+
 function passe(pass) {
     left = left - pass;
-    cards.style.left = `${left}px`;    
+    cards.style.left = `${left}px`;
 }
 
 function retornar(x, y) {    
-    if (left <= -x) {
+    if (left == y) {
+        console.log("ok");
+    } else if (left <= -x) {
         left = y;
         cards.style.left = `${y}px`;
-    }
+    }  
+    
+    
 };
 
 function turnRight() {
     if (screen >= 1441) {
-        passe(220);
-        retornar(875, 40);
+        passe(220.7);
+        retornar(879, 40);
     } else if (screen >= 1201 && screen < 1441){
-        passe(220);
-        retornar(1075, 40);
+        passe(235);
+        retornar(1140, 40);
     } else if (screen >= 769 && screen < 1200){
-        passe(220);
-        retornar(1501, 40);
+        passe(220.7);
+        retornar(1506, 40);
     } else if (screen >= 426 && screen < 769) {
         passe(220.7);
         retornar(1726, 40);
@@ -51,5 +57,34 @@ function turnRight() {
     }else if (screen < 321) {
         passe(250);
         retornar(2500, 15);
+    }
+}
+
+function turnLeft() {
+    if (left <= 70 && left >= 0) {
+        console.log("Erro!");
+    } else{
+        if (screen >= 1441) {
+            passe(-220.7);
+            retornar(right, 40);
+        } else if (screen >= 1201 && screen < 1441){
+            passe(-235);
+            retornar(right, 40);
+        }else if (screen >= 769 && screen < 1200){
+            passe(-220.7);
+            retornar(right, 40);
+        } else if (screen >= 426 && screen < 769) {
+            passe(-220.7);
+            retornar(right, 40);
+        } else if (screen >= 376 && screen < 426) {
+            passe(-250);
+            retornar(right, 70);
+        }else if (screen >= 321 && screen < 376) {
+            passe(-250);
+            retornar(right, 43);
+        }else if (screen < 321) {
+            passe(-250);
+            retornar(right, 15);
+        }
     }
 }
